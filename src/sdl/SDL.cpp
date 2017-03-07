2332d2214545917b74cd4fc17c508eb8ec2793e3
Emanuel Strobel <emanuel@ihad.tv>
2017-03-07 14:11:34 +0100

    VisualBoyAdvance: remove a forgotten one

--- src/sdl/SDL.cpp
+++ src/sdl/SDL.cpp
@@ -16,6 +16,11 @@
 // along with this program; if not, write to the Free Software Foundation,
 // Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
+//
+// Parts of the audio, video and timing loops
+// Copyright (c) 2012 Lenny <leonardo.masoni@gmail.com>
+//
+
 #include <stdarg.h>
 #include <stdlib.h>
 #include <stdio.h>
